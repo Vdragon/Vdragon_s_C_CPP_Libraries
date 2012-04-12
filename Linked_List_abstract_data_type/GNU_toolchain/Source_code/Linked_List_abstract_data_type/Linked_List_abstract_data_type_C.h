ab8f2dcd7d3c49df0f9e06249458026bb9b67bbf
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-04-12 15:24:44 +0800

    更新Linked_List_abstract_data_type：建立專案

--- Linked_List_abstract_data_type/GNU_toolchain/Source_code/Linked_List_abstract_data_type/Linked_List_abstract_data_type_C.h
+++ Linked_List_abstract_data_type/GNU_toolchain/Source_code/Linked_List_abstract_data_type/Linked_List_abstract_data_type_C.h
@@ -1,7 +1,11 @@
+/*list element*/
+typedef int List_element;
+
+
 /*單向Linked list節點資料結構*/
 typedef struct uniListNode{
 	/*資料宣告*/
-
+  List_element data;
 	/*指向下一節點的指標*/
 	struct uniListNode * next_node;
 }UniListNode;
@@ -11,7 +15,7 @@ typedef struct biListNode{
 	/*指向前一節點的指標*/
 	struct biListNode * before_node;
 	/*資料宣告*/
-
+  List_element data;
 	/*指向下一節點的指標*/
 	struct biListNode * next_node;
 }BiListNode;
