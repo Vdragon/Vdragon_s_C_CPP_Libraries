c70c15bb6cc34e7730ab0d13b8dc1a2937885f96
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-16 04:45:59 +0800

    更新Tree_abstract_data_type：建立專案

---
+++ Tree_abstract_data_type/GNU_toolchain/Source_code/Test.cpp
@@ -0,0 +1,53 @@
+/*Test.cpp
+-----------------------------------
+更新紀錄 | Changelog
+  Changelog is now stored on GitHub
+已知問題 | Known Issues
+  Known issues is now stored on GitHub
+待辦事項 | Todos
+  Todo is now stored on GitHub
+著作權宣告 | Copyright notice
+  Copyright 2012 林博仁(Henry Lin, pika1021@gmail.com)
+智慧財產授權條款：
+  Test.cpp is part of Tree_abstract_data_type
+  Tree_abstract_data_type is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Tree_abstract_data_type is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Tree_abstract_data_type.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*--------------程式碼開始(Code Started)--------------*/
+/*--------------前期處理器指令(Preprocessor Directive)--------------*/
+/*////////程式所include之函式庫的標頭檔(Included Library Headers)////////*/
+/* 標準C++函式庫 */
+/* EXIT_SUCCESS */
+#include <cstdlib>
+
+/* this */
+#include "Tree_abstract_data_type/Tree_abstract_data_type_CPP.h"
+
+/*////////常數與巨集(Constants & Macros)////////*/
+
+/*////////其他前期處理器指令(Other Preprocessor Directives////////*/
+
+/*--------------全域宣告與定義(Global Declaration & Definition)--------------*/
+/*////////Classes、資料結構(Data Structures)、type definitions跟enumerations////////*/
+
+/*////////函式雛型(Function Prototypes)////////*/
+
+/*////////全域變數(Global Variables)////////*/
+
+/*--------------主要程式碼(Main Code)--------------*/
+int main(void)
+{
+
+  return EXIT_SUCCESS;
+}
