abe7211430bef410cd941ff7bda22b3488dda6e4
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-16 04:55:32 +0800

    更新Tree_abstract_data_type/：修正部份不合法的註解（未完成）

--- Tree_abstract_data_type/GNU_toolchain/Source_code/Tree_abstract_data_type/Tree_abstract_data_type_C.c
+++ Tree_abstract_data_type/GNU_toolchain/Source_code/Tree_abstract_data_type/Tree_abstract_data_type_C.c
@@ -1,136 +1,98 @@
-﻿
-/*指向二元樹根節點的指標*/
+﻿/*指向二元樹根節點的指標*/
 /*TreeNode * treeRoot1 = NULL;*/
-
 
 /*中序Traversal演算法函式
   版本：1.00(0)
   回傳值的資料型式 函式名稱(參數定義)
-  returnType functionName(definition of parameters)
-*/
+  returnType functionName(definition of parameters)*/
 void inorder(TreeNode * treeNodePtr)
 	{
-        /*宣告與定義(Declaration & Definition)*/
+  /*宣告與定義(Declaration & Definition)*/
 
 
-        /*－－－－－－－－－－－－－－－－－－－－－*/
-        /*1.直到到達leaf節點即停止呼叫自己*/
-        if(treeNodePtr != NULL){
-            /*1.1.先呼叫自己拜訪左子樹*/
-            inorder(treeNodePtr->leftChild);
-            /*1.2.讀出值*/
+  /*－－－－－－－－－－－－－－－－－－－－－*/
+  /*1.直到到達leaf節點即停止呼叫自己*/
+  if(treeNodePtr != NULL){
+      /*1.1.先呼叫自己拜訪左子樹*/
+      inorder(treeNodePtr->leftChild);
+      /*1.2.讀出值*/
 
-            /*1.3.再呼叫自己拜訪右子樹*/
-            inorder(treeNodePtr->rightChild);
-        }/*1.直到到達leaf節點即停止呼叫自己
+      /*1.3.再呼叫自己拜訪右子樹*/
+      inorder(treeNodePtr->rightChild);
+  }/*1.直到到達leaf節點即停止呼叫自己
 
-        /*－－－－－－－－－－－－－－－－－－－－－*/
-        /*傳回內容*/
-        return ;
+  /*－－－－－－－－－－－－－－－－－－－－－*/
+  /*傳回內容*/
+  return ;
 	}
 
 
-//前序Traversal演算法函式
-//版本：0.00(0)
-/*
-回傳值的資料型式 函式名稱(參數定義)
-returnType functionName(definition of parameters)
-*/
+/* 前序Traversal演算法函式 */
 void preorder(TreeNode * treeNodePtr)
-	{
-        //宣告與定義(Declaration & Definition)
+{
+
+  /* 1.直到到達leaf節點即停止呼叫自己 */
+  if(treeNodePtr != NULL){
+    /* 1.1.先讀出值 */
+
+    /* 1.2.再呼叫自己拜訪左子樹 */
+    preorder(treeNodePtr->leftChild);
+    /* 1.3.再呼叫自己拜訪右子樹 */
+    preorder(treeNodePtr->rightChild);
+  }
+  return ;
+}
 
 
-        //－－－－－－－－－－－－－－－－－－－－－
-        //1.直到到達leaf節點即停止呼叫自己
-        if(treeNodePtr != NULL){
-            //1.1.先讀出值
-
-            //1.2.再呼叫自己拜訪左子樹
-            preorder(treeNodePtr->leftChild);
-            //1.3.再呼叫自己拜訪右子樹
-            preorder(treeNodePtr->rightChild);
-        }////1.直到到達leaf節點即停止呼叫自己
-
-        //－－－－－－－－－－－－－－－－－－－－－
-        //傳回內容
-        return ;
-	}
-
-
-//後序Traversal演算法函式
-//版本：0.00(0)
-/*
-回傳值的資料型式 函式名稱(參數定義)
-returnType functionName(definition of parameters)
-*/
+/* 後序Traversal演算法函式 */
 void postorder(TreeNode * treeNodePtr)
-	{
-        //宣告與定義(Declaration & Definition)
+{
+  /*1.直到到達leaf節點即停止呼叫自己*/
+  if(treeNodePtr != NULL){
+    /*1.1.先呼叫自己拜訪左子樹*/
+    postorder(treeNodePtr->leftChild);
+    /*1.2.再呼叫自己拜訪右子樹*/
+    postorder(treeNodePtr->rightChild);
+    /*1.3.再讀出值*/
+
+  }
+  return ;
+}
 
 
-        //－－－－－－－－－－－－－－－－－－－－－
-        //1.直到到達leaf節點即停止呼叫自己
-        if(treeNodePtr != NULL){
-            //1.1.先呼叫自己拜訪左子樹
-            postorder(treeNodePtr->leftChild);
-            //1.2.再呼叫自己拜訪右子樹
-            postorder(treeNodePtr->rightChild);
-            //1.3.再讀出值
-
-        }////1.直到到達leaf節點即停止呼叫自己
-
-        //－－－－－－－－－－－－－－－－－－－－－
-        //傳回內容
-        return ;
-	}
-
-
-//用level order形式印出heap中的資料的函式
-//版本：1.00(0)
+/* 用level order形式印出heap中的資料的函式 */
 void levelPrintHeap(HeapElement heap[], unsigned int heap_size)
     {
-    //宣告與定義(Declaration & Definition)
-    //現在處理的元素計數器
+    /*宣告與定義(Declaration & Definition)*/
+    /*現在處理的元素計數器*/
     register unsigned curr_print;
 
-    //－－－－－－－－－－－－－－－－－－－－－
-    //如果heap中無資料則直接退出
+    /*－－－－－－－－－－－－－－－－－－－－－*/
+    /*如果heap中無資料則直接退出*/
     if(HEAP_IS_EMPTY(heap_size)){
         printf("目前heap中無資料。\n"
                "There are currently no data in the heap.\n");
         return;
     }
     else{
-        //用for迴圈從第一個元素輸出到最後一個元素
+        /*用for迴圈從第一個元素輸出到最後一個元素*/
         for(curr_print = 1; curr_print <= heap_size; curr_print++){
             printf("%d[%d] ", curr_print, heap[curr_print].key);
         }
     }
-
-    //斷行
+    /*斷行*/
     putchar('\n');
 
-    //－－－－－－－－－－－－－－－－－－－－－
-    //結束函式
     return ;
     }
 
 
-//Max Heap目前的元素數量
-/*unsigned int heap_size = 0;*/
-
-//Max Heap的宣告
-/*HeapElement max_heap[MAX_HEAP_ELEMENTS];*/
-
-
-//Push元素至Max Heap中的函式
-//版本：1.00(0)
-//時間複雜度：O(log(2,n))
+/* Push元素至Max Heap中的函式
+   時間複雜度：O(log(2,n)) */
 short int maxHeapPush(HeapElement item, HeapElement max_heap[], unsigned int *heap_size)
     {
-    //宣告與定義(Declaration & Definition)
-    //元素的寫入位置
+    /*宣告與定義(Declaration & Definition)*/
+    /* 元素的寫入位置 */
     unsigned int insert_position;
 
     //－－－－－－－－－－－－－－－－－－－－－
@@ -191,24 +153,24 @@ HeapElement maxHeapPop(HeapElement max_heap[], unsigned int *heap_size)
 
     //當還沒有判斷完全部的child之前
     while(child_compare <= *heap_size){
-        //如果child位置還沒到最後一個元素且當前parent的右邊child的key比左邊child的key還要大
-        if((child_compare < *heap_size) && (max_heap[child_compare].key < max_heap[child_compare + 1].key)){
-            //將child_compare設成兩個child中比較大者
-            child_compare++;
-        }
+      //如果child位置還沒到最後一個元素且當前parent的右邊child的key比左邊child的key還要大
+      if((child_compare < *heap_size) && (max_heap[child_compare].key < max_heap[child_compare + 1].key)){
+          //將child_compare設成兩個child中比較大者
+          child_compare++;
+      }
 
-        //如果temp的key比兩個child中比較大的child的key還要大表示temp應該插入至這個位置
-        if(temp.key >= max_heap[child_compare].key){
-            //離開while迴圈
-            break;
-        }
+      //如果temp的key比兩個child中比較大的child的key還要大表示temp應該插入至這個位置
+      if(temp.key >= max_heap[child_compare].key){
+          //離開while迴圈
+          break;
+      }
 
-        //將兩個child中比較大的child swap至其parent（當前因為被pop掉了所以無值）的位置
-        max_heap[parent_insert] = max_heap[child_compare];
-        parent_insert = child_compare;
+      //將兩個child中比較大的child swap至其parent（當前因為被pop掉了所以無值）的位置
+      max_heap[parent_insert] = max_heap[child_compare];
+      parent_insert = child_compare;
 
-        //換成兩個child中比較大的child的child節點繼續跟temp做key的比較
-        child_compare *= 2;
+      //換成兩個child中比較大的child的child節點繼續跟temp做key的比較
+      child_compare *= 2;
     }//結束while判斷迴圈
 
     //將temp插入至當前無值的位置
@@ -229,39 +191,34 @@ TreeNode * dataToTree(TreeNode * * target_root, const char * input_string)
 		//－－－－－－－－－－－－－－－－－－－－－
 		/*test 01
 		printf("%d\t%d\n", '0', '9');
-        *///test 01->48, 57
+    *///test 01->48, 57
 
-        //malloc current root node
-        *target_root = (TreeNode *)malloc(sizeof(TreeNode));
+    //malloc current root node
+    *target_root = (TreeNode *)malloc(sizeof(TreeNode));
 
-        //如果得到的string中間項是一個數字表示該為leaf節點
-        if(input_string[0] >= 48 && input_string[0] <= 57){
-            //將target_root值設為該數字，其左右子設NULL
-            (*target_root)->data = atoi(input_string);
-            (*target_root)->leftChild = NULL;
-            (*target_root)->rightChild = NULL;
-        }
-        else{//得到的string非數字（左括號）
-            //第二個字元為運算子
-            (*target_root)->data = input_string[1];
-            //第三個字元之後（第四個字元開始）的字串是左算式
-            (*target_root)->leftChild = dataToTree(&(*target_root)->leftChild, input_string + 3);
+    //如果得到的string中間項是一個數字表示該為leaf節點
+    if(input_string[0] >= 48 && input_string[0] <= 57){
+        //將target_root值設為該數字，其左右子設NULL
+        (*target_root)->data = atoi(input_string);
+        (*target_root)->leftChild = NULL;
+        (*target_root)->rightChild = NULL;
+    }
+    else{//得到的string非數字（左括號）
+        //第二個字元為運算子
+        (*target_root)->data = input_string[1];
+        //第三個字元之後（第四個字元開始）的字串是左算式
+        (*target_root)->leftChild = dataToTree(&(*target_root)->leftChild, input_string + 3);
 
-            //呼叫findRightFormula尋找右運算式位置
-            (*target_root)->rightChild = dataToTree(&(*target_root)->rightChild, input_string + findRightFormula(input_string));
-        }
+        //呼叫findRightFormula尋找右運算式位置
+        (*target_root)->rightChild = dataToTree(&(*target_root)->rightChild, input_string + findRightFormula(input_string));
+    }
 		//－－－－－－－－－－－－－－－－－－－－－
 		//傳回內容
 		return *target_root;
 	}
 
 
-//判斷右算式位置的函式
-//版本：0.00(0)
-/*
-回傳值的資料型式 函式名稱(參數定義)
-returnType functionName(definition of parameters)
-*/
+/* 判斷右算式位置的函式 */
 inline short int findRightFormula(const char * input_string)
     {
     //宣告與定義(Declaration & Definition)
@@ -272,20 +229,20 @@ inline short int findRightFormula(const char * input_string)
     //－－－－－－－－－－－－－－－－－－－－－
     //當還沒讀到字串結尾時
     while(input_string[read_string_count] != '\0'){
-        switch(input_string[read_string_count]){
-        case '(':
-            left_brace_count++;
-            break;
-        case ')':
-            right_brace_count++;
-            break;
-        case ',':
-            comma_count++;
-            break;
-        default:
-            break;
-        };
-        read_string_count++;
+      switch(input_string[read_string_count]){
+      case '(':
+          left_brace_count++;
+          break;
+      case ')':
+          right_brace_count++;
+          break;
+      case ',':
+          comma_count++;
+          break;
+      default:
+          break;
+      };
+      read_string_count++;
     }
 
     //判斷formula類型
@@ -332,27 +289,23 @@ inline short int findRightFormula(const char * input_string)
     }
 
 
-//清除樹的記憶體配置函式
-//版本：0.00(0)
+/* 清除樹的記憶體配置函式 */
 TreeNode * destroyTree(TreeNode * treeNodePtr)
-    {
-    //宣告與定義(Declaration & Definition)
+  {
 
+  //1.直到到達leaf節點即停止呼叫自己
+  if(treeNodePtr != NULL){
+      //1.1.先呼叫自己拜訪左子樹
+      destroyTree(treeNodePtr->leftChild);
+      //1.2.清除左子樹節點
+      free(treeNodePtr->leftChild);
+      //1.3.再呼叫自己拜訪右子樹
+      destroyTree(treeNodePtr->rightChild);
+      //1.4.清除右子樹節點
+      free(treeNodePtr->rightChild);
+  }////1.直到到達leaf節點即停止呼叫自己
 
-    //－－－－－－－－－－－－－－－－－－－－－
-    //1.直到到達leaf節點即停止呼叫自己
-    if(treeNodePtr != NULL){
-        //1.1.先呼叫自己拜訪左子樹
-        destroyTree(treeNodePtr->leftChild);
-        //1.2.清除左子樹節點
-        free(treeNodePtr->leftChild);
-        //1.3.再呼叫自己拜訪右子樹
-        destroyTree(treeNodePtr->rightChild);
-        //1.4.清除右子樹節點
-        free(treeNodePtr->rightChild);
-    }////1.直到到達leaf節點即停止呼叫自己
-
-    //－－－－－－－－－－－－－－－－－－－－－
-    //傳回內容
-    return treeNodePtr;
-    }
+  //－－－－－－－－－－－－－－－－－－－－－
+  //傳回內容
+  return treeNodePtr;
+  }
