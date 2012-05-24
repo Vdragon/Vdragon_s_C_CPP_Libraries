c4cb254e8674c33ff76d4ead00f05025301f5ac6
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-05-25 02:12:41 +0800

    完成將heapsort改變為使用Tree ADT的物件的實作

---
+++ Sorting_algorithm/Source_code/Sorting_algorithm/Tree_abstract_data_type_Int/Tree_abstract_data_type_C.c
@@ -0,0 +1,163 @@
+﻿/* ====標準C函式庫==== */
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+/* this */
+#include "Tree_abstract_data_type_C.h"
+
+/* Vdragon's Library Collection */
+#include "../../Messages_templates/zh_TW.h"
+#include "Swap_algorithm_heap_element/Swap_algorithm.h"
+
+/* ====堆抽象資料類型 | Heap Abstract Data Structure==== */
+/* 建構並初始化heap物件的函式 */
+short heapCreate(Heap *self, unsigned size, HeapType type)
+{
+  /* 初始化各個函式指標 */
+  self->destroy = heapDestroy;
+  self->isEmpty = heapIsEmpty;
+  self->isFull = heapIsFull;
+
+  /* 建立heap[1~size] */
+  self->heap = (HeapElement *)malloc(sizeof(HeapElement) * size + 1);
+  if(self->heap == NULL){
+    fprintf(stderr, ERROR_TAG ERROR_MEMORY_ALLOCATION_FAIL);
+    return -1;
+  }
+
+  /* 初始化屬性 */
+  self->length = 0;
+  self->size = size;
+  self->type = type;
+
+  /* 完成create操作 */
+  return 0;
+}
+
+/* 清除heap物件的函式 */
+void heapDestroy(Heap *self)
+{
+  /* 釋放記憶體空間 */
+  free(self->heap);
+  self->size = self->length = 0;
+
+  /* 完成destroy操作 */
+  return;
+}
+
+/* 判斷heap是否為空的函式 */
+short heapIsEmpty(Heap *self)
+{
+  if(self->length == 0){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+/* 判斷heap是否為滿的函式 */
+short heapIsFull(Heap *self)
+{
+  if(self->length == self->size){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+/* 產生父母節點索引值的inline函式 */
+unsigned INDEX_PARENT(const unsigned child_index)
+{
+  return child_index >> 1;
+}
+/* 產生左子節點索引值的inline函式 */
+unsigned INDEX_LEFT_CHILD(const unsigned parent_index)
+{
+  return parent_index << 1;
+}
+
+/* 產生右節點索引值的inline函式 */
+unsigned INDEX_RIGHT_CHILD(const unsigned parent_index)
+{
+  return (parent_index << 1) + 1;
+}
+
+/*Heapify函式
+    When assuming the left and right children node are heaps,
+    let the complete binary tree rooted by parent node be a heap.
+  參數
+  　parent_index
+  　　父母節點的索引值（可能不符合heap性質）*/
+void heapHeapify(Heap *self, const unsigned parent_index)
+{
+  /*current largest node, preassuming parent*/
+  unsigned largest_index = parent_index;
+
+  /*the child index of current node may be*/
+  unsigned left_child_index = INDEX_LEFT_CHILD(largest_index),
+            right_child_index = INDEX_RIGHT_CHILD(largest_index);
+  /*－－－－－－－－－－－－－－－－－－－－－*/
+
+  /*if left child exist and its key greater than current node*/
+  if(left_child_index <= self->length &&
+     ((self->type == MAX_HEAP)?
+         self->heap[left_child_index] > self->heap[largest_index]:
+         self->heap[left_child_index] < self->heap[largest_index])){
+     largest_index = left_child_index;
+  }
+
+  /*if right child exist and greater than current node*/
+  if(right_child_index <= self->length &&
+     ((self->type == MAX_HEAP)?
+         self->heap[right_child_index] > self->heap[largest_index]:
+         self->heap[right_child_index] < self->heap[largest_index])){
+     largest_index = right_child_index;
+  }
+
+  /*if largest node isn't current node then swap with the largest
+    then maxheapify the child which gets the parent node(which may violating the heap property)*/
+  if(largest_index != parent_index){
+    swapHeapElement(&(self->heap[parent_index]), &(self->heap[largest_index]));
+    heapHeapify(self, largest_index);
+  }
+  /* 完成 */
+  return;
+}
+
+/*buildHeap function
+  build a heap by calling Heapify from the last
+  non-leaf node to root node*/
+void heapBuildHeap(Heap *self)
+{
+  unsigned parent;
+
+  /*for last non-leaf node to root*/
+  for (parent = (self->length / 2); parent >= 1; --parent) {
+    heapHeapify(self, parent);
+  }
+
+  /*done*/
+  return;
+}
+
+/* Add元素至Heap中的函式 */
+short heapAdd(Heap *self, HeapElement item)
+{
+  /*如果heap已滿就異常退出*/
+  if(self->length == self->size){
+    fprintf(stderr, ERROR_TAG "無法新增元素於已滿的heap！");
+    return -1;
+  }
+
+  /* 將欲加入的元素放置於heap的尾端之後 */
+  self->heap[++self->length] = item;
+
+  /* 對已經不維持heap性質的陣列重新構築heap */
+  heapBuildHeap(self);
+
+  /* 完成add操作 */
+  return 0;
+}
+
---
+++ Sorting_algorithm/Source_code/Sorting_algorithm/Tree_abstract_data_type_Int/Tree_abstract_data_type_C.h
@@ -0,0 +1,142 @@
+﻿#ifndef TREE_DATA_STRUCTURE_ALGORITHM_H_INCLUDED
+  #define TREE_DATA_STRUCTURE_ALGORITHM_H_INCLUDED
+  #ifdef __cplusplus
+    extern "C"{
+  #endif
+
+  /* 訊息 */
+  #define TREE_ADT_TAG "【樹(Tree)抽象資料類型】"
+  #define HEAP_ADT_TAG "【Heap抽象資料類型】"
+
+  /* ====樹(tree)抽象資料類型定義==== */
+  /* 樹節點的定義 */
+  typedef struct binaryTreeNode{
+    /*資料項*/
+    int data;
+    /*指向左右子節點的指標*/
+    struct binaryTreeNode *leftChild, *rightChild;
+  }BinaryTreeNode;
+
+  /* 典型二元樹抽象資料類型 */
+  typedef struct binaryTree{
+    /* ==屬性== */
+    /* 二元樹的根節點 */
+    BinaryTreeNode *root;
+
+    /* ==介面== */
+    /* 初始化二元樹物件的函式 */
+    void (*create)(struct binaryTree *self);
+    /* 清除二元樹物件的函式 */
+    void (*destroy)(struct binaryTree *self);
+    /* 中序Traversal演算法函式 */
+    void (*inorder)(struct binaryTree *self);
+    /* 前序Traversal演算法函式 */
+    void (*preorder)(struct binaryTree *self);
+    /* 後序Traversal演算法函式 */
+    void (*postorder)(struct binaryTree *self);
+    /* 將3-tuple資料寫入樹中的函式 */
+    BinaryTreeNode * (*dataToTree)(struct binaryTree *self, const char *input);
+    /* 元件測試函式 */
+    short (*unitTest)(struct binaryTree *self);
+
+  }BinaryTree;
+  /* ====public member functions==== */
+  short binaryTreeUnitTest(BinaryTree *self);
+  void binaryTreeCreate(BinaryTree *self);
+  void binaryTreeDestroy(BinaryTree *self);
+  void binaryTreeInorder(BinaryTree *self);
+  void binaryTreePreorder(BinaryTree *self);
+  void binaryTreePostorder(BinaryTree *self);
+  void binaryTreeDataToTree(BinaryTree *self, const char *input);
+
+  /* ====private member functions==== */
+  /* 遞迴地釋放樹佔用的記憶體的函式
+   * 運行完後只剩下最上層的root還存在 */
+  void destroyBinaryTreeChild(BinaryTreeNode *root);
+  void inorder(BinaryTreeNode *root);
+  void preorder(BinaryTreeNode *root);
+  void postorder(BinaryTreeNode * treeNodePtr);
+  BinaryTreeNode * dataToBinaryTree(BinaryTreeNode * *target_root, const char *input);
+  /* 判斷右算式位置的函式 */
+  short findRightFormula(const char * input);
+
+
+/* ====堆(heap)抽象資料類型定義====
+ * 1.本class以陣列(array)來實作heap，結構如下：
+ *   不使用 最大／最小元素 比[1]大／小的元素 比[2]大／小的元素 比[3]大／小的元素 ...
+ *     0        1           2 & 3           4 & 5           6 & 7      ...
+ *            1    <---根節點
+ *          2   3
+ *         4 5 6 7
+ **/
+/* heap元素的定義 */
+typedef int HeapElement;
+
+/* Heap的類型定義 */
+typedef enum heapType{
+  MAX_HEAP, MIN_HEAP
+}HeapType;
+
+/* Heap的class定義 */
+typedef struct heap{
+  /* ====屬性==== */
+  /* heap的類型(max-heap/min-heap) */
+  HeapType type;
+  /* heap陣列
+   * 由heap[1]至heap[size] */
+  HeapElement *heap;
+  /* heap陣列的總大小（不包括下標(subscript)為零者） */
+  unsigned size;
+  /* 目前heap使用的元素數（不包括下標(subscript)為零者） */
+  unsigned length;
+
+  /* ====介面==== */
+  /* 建構並初始化heap物件的函式
+   * 參數
+   * 　*self
+   * 　　物件本身
+   * 　size
+   * 　　heap大小
+   * 回傳數值
+   * 　-1
+   * 　　系統可用記憶體空間不足*/
+  short (*create)(struct heap *self, unsigned size, HeapType type);
+  /* 清除heap物件的函式 */
+  void (*destroy)(struct heap *self);
+  /* 判斷heap是否為空的函式 */
+  short (*isEmpty)(struct heap *self);
+  /* 判斷heap是否為滿的函式 */
+  short (*isFull)(struct heap *self);
+}Heap;
+/* ==private成員函式== */
+/* 產生父母節點索引值的inline函式 */
+unsigned INDEX_PARENT(const unsigned parent_index);
+/* 產生左子節點索引值的inline函式 */
+unsigned INDEX_LEFT_CHILD(unsigned parent_index);
+/* 產生右節點索引值的inline函式 */
+unsigned INDEX_RIGHT_CHILD(unsigned parent_index);
+/*將子節點為heap但根節點未必遵守heap性質的完整二元樹變為heap的函式
+    When assuming the left and right children node are max heaps,
+    let the complete binary tree rooted by parent node be a max heap.
+  參數
+  　parent_index
+  　　父母節點的索引值（可能不符合heap性質）*/
+void heapHeapify(Heap *self, const unsigned parent_index);
+/*heapBuildHeap function
+  build a heap by calling Heapify from the last
+  non-leaf node to root node*/
+void heapBuildHeap(Heap *self);
+
+/* ==public 成員函式== */
+short heapCreate(Heap *self, unsigned size, HeapType type);
+void heapDestroy(Heap *self);
+short heapIsEmpty(Heap *self);
+short heapIsFull(Heap *self);
+void heapLevelPrint(Heap *self);
+short heapAdd(struct heap *self, HeapElement item);
+HeapElement heapDelete(Heap *self, short *result);
+
+#ifdef __cplusplus
+    }
+  #endif
+#endif
