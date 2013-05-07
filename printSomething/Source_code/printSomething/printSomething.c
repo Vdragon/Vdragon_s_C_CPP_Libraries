aafd7e7830f9e0ecc0b25eea2f8555d58535b4d4
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-05-08 00:24:05 +0800

    [printSomething]新增範本

---
+++ printSomething/Source_code/printSomething/printSomething.c
@@ -0,0 +1,27 @@
+/* 
+	請見本檔案的 header 檔案以得到更多關於本檔案的說明
+	
+	本程式的框架基於「C/C++ program templates」專案
+	This program's frame is based on "C/C++ program templates" project
+		https://github.com/Vdragon/C_CPP_program_templates
+	建議編輯器設定
+	Recommended editor settings
+		Indentation by tab character
+		Tab character width = 2 space characters
+*/
+/* 程式所 include 之函式庫的標頭檔
+ * Included Library Headers */
+
+/* 常數與巨集
+ * Constants & Macros */
+
+/* Definition of data type, enumeration, data structure and class */
+
+/* 函式雛型
+ * Function Prototypes */
+
+/* 全域變數
+ * Global Variables */
+
+/* 主要程式碼
+ * Main Code */
